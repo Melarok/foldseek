7e5165b45b3a5406924bf0c725992382847a3b2a
Melarok <hendrik.frueh@gmail.com>
2023-06-14 17:44:21 +0200

    improve entrypoint

--- util/foldseekRun.sh
+++ util/foldseekRun.sh
@@ -1,8 +1,10 @@
 #! /bin/bash
 
 for query in /mnt/query/*.pdb; do
+    qName=$(basename "$query" .pdb)
     for db in /mnt/db/*.DB; do
-        echo "Processing query $(basename "$query") on database $(basename "$db")"
-        /usr/local/bin/foldseek_avx2 easy-search "$query" "$db" /mnt/results/"$query"-"$db" /tmp --threads 16 -v 1
+        dName=$(basename "$db" .DB)
+        echo -e "\nProcessing query $qName on database $dName"
+        /usr/local/bin/foldseek_avx2 easy-search "$query" "$db" /mnt/results/"$qName"-"$dName".html /tmp --threads 16 -v 1 --format-mode 3
     done
 done
