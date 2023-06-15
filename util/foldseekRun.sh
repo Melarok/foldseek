14171f9c374b2ae61dc7d59efff93b55a0914413
Melarok <hendrik.frueh@gmail.com>
2023-06-15 14:50:07 +0200

    entrypoint improvements

--- util/foldseekRun.sh
+++ util/foldseekRun.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-/usr/local/bin/foldseek_avx2 createdb /mnt/query/* querydb --threads 16
+/usr/local/bin/foldseek_avx2 createdb /mnt/query/* querydb --threads 16 -v 1
 
 results="/mnt/results/$(date -Iminutes)"
-mkdir -p "$results"
+mkdir -p "$results"/raw "$results"/processed
 
 for db in /mnt/db/*.DB; do
     dName=$(basename "$db" .DB)
     echo -e "\nProcessing querydb on targetdb $dName"
-    /usr/local/bin/foldseek_avx2 search querydb "$db" "$results"/"$dName" /tmp -e 0.005 --threads 16
-    /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$results"/"$dName" "$results"/"$dName".m8 --threads 16
-    /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$results"/"$dName" "$results"/"$dName".html --format-output 3 --threads 16
-    /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$results"/"$dName" "$results"/"$dName".pdb --format-output 5 --threads 16
+    /usr/local/bin/foldseek_avx2 search querydb "$db" "$results"/raw/"$dName".res /tmp -e 0.005 --threads 16 -v 1
+    /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$results"/raw/"$dName".res "$results"/processed/"$dName".m8 --threads 16 -v 1
+    /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$results"/raw/"$dName".res "$results"/processed/"$dName".html --format-mode 3 --threads 16 -v 1
+    /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$results"/raw/"$dName".res "$results"/processed/"$dName".pdb --format-mode 5 --threads 16 -v 1
 done
