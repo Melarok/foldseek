eabb9f98f04a7ce7c124be0f6c151dac50ac9b05
Melarok <hendrik.frueh@gmail.com>
2023-06-15 16:07:52 +0200

    add -a to foldseek search

--- util/foldseekRun.sh
+++ util/foldseekRun.sh
@@ -9,7 +9,7 @@ for db in /mnt/db/*.DB; do
     dName=$(basename "$db" .DB)
     result="$resultFolder"/raw/"$(basename "$db")".res
     echo -e "\nProcessing querydb on targetdb $dName"
-    /usr/local/bin/foldseek_avx2 search querydb "$db" "$result" /tmp -e 0.005 --threads 16 -v 1
+    /usr/local/bin/foldseek_avx2 search querydb "$db" "$result" /tmp -e 0.005 --threads 16 -v 1 -a
     /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$result" "$resultFolder"/processed/"$dName".m8 --threads 16 -v 1
     /usr/local/bin/foldseek_avx2 convertalis querydb "$db" "$result" "$resultFolder"/processed/"$dName".html --format-mode 3 --threads 16 -v 1
 done
