fcd89d8b7cc548be7eb7e2484a50f016190ba4a3
Melarok <hendrik.frueh@gmail.com>
2023-06-15 12:42:51 +0200

    Only return hits with an E-Value below 0.005

--- util/foldseekRun.sh
+++ util/foldseekRun.sh
@@ -6,6 +6,6 @@ for query in /mnt/query/*.pdb; do
     for db in /mnt/db/*.DB; do
         dName=$(basename "$db" .DB)
         echo -e "\nProcessing query $qName on database $dName"
-        /usr/local/bin/foldseek_avx2 easy-search "$query" "$db" /mnt/results/"$qName"/"$qName"-"$dName".html /tmp --threads 16 -v 1 --format-mode 3
+        /usr/local/bin/foldseek_avx2 easy-search "$query" "$db" /mnt/results/"$qName"/"$qName"-"$dName".html /tmp --threads 16 -v 1 --format-mode 3 -e 0.005
     done
 done
